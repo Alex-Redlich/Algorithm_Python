00473562c6af07aa5cd5d16c25b1695bf43cbdb5
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2023-03-03 15:46:19 +0900

    [Bronze I] Title: 색종이, Time: 228 ms, Memory: 38304 KB -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,17 +1,17 @@
 N = int(input())
-arr = [[0] * 1002 for _ in range(1002)]
+arr = [[0] * 1001 for _ in range(1001)]
 for n in range(1,N+1):
     x,y,h,v = map(int, input().split())
     for i in range(x,x+h):
         for j in range(y,y+v):
             arr[i][j] = n
-            
-cnt_lst = [0] * (N+1)
-for n in range(1, N+1):
-    for i in range(1002):
-        for j in range(1002):
-            if arr[i][j] == n:
-                cnt_lst[n] += 1
 
-for x in range(1,N+1):
-    print(cnt_lst[x])
+cnt_lst = [0] * (N+1)
+for lst in arr:
+    for n in lst:
+        cnt_lst[n] += 1
+        
+print(*cnt_lst[1:], sep='\n')
+
+
+
