443621f5338eec0c36828632826558b312a130d7
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2023-04-12 16:04:03 +0900

    [Bronze I] Title: 이항 계수 1, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,12 @@
+def factorial(n):
+    ans = 1
+    for i in range(2, n+1):
+        ans *= i
+    return ans
+
+
+def bino_coef_factorial(n, r):
+    return factorial(n) // factorial(r) // factorial(n-r)
+
+N, K = map(int,input().split())
+print(bino_coef_factorial(N,K))
