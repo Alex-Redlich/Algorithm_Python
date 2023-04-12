3a36947dac4583610e9de0caf82642102e41a9cb
KIM YOUNGMIN <122436515+Alex-Redlich@users.noreply.github.com>
2023-04-12 16:20:58 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 4232 ms, Memory: 54084 KB -BaekjoonHub

---
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -0,0 +1,9 @@
+N = int(input())
+lst = []
+for _ in range(N):
+    x,y = map(int,input().split())
+    lst.append([x,y])
+
+lst.sort(key=lambda x : (x[0],x[1]))
+for i in range(N):
+    print(lst[i][0], lst[i][1])
